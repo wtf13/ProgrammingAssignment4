4ff106dd1b81d6436d2586c57bb0fe5f5c0665e4
wtg13 <wantingg@gmail.com>
2017-08-10 01:47:42 +0800

    update r script

--- run_analysis.R
+++ run_analysis.R
@@ -30,10 +30,38 @@ dat<-rbind(testdat,traindat)
 
 ##2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
-##creating logical vector indicating only activity, subject, mean and sd variables 
+##creating logical vector indicating only activityid, subjectid, mean and sd variables 
 cols<-colnames(dat)
-wantedvars<-grepl("activityid|subjectid|mean|std",cols)
+wantedvars<-(grepl("activityid|subjectid|mean|std",cols) & !grepl("meanFreq",cols))
+meanstddat<-dat[wantedvars]
 
-##Uses descriptive activity names to name the activities in the data set
-##Appropriately labels the data set with descriptive variable names.
-##From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+##3.Uses descriptive activity names to name the activities in the data set
+meanstddat<-merge(meanstddat,activitytype,by="activityid")
+newcols<-colnames(meanstddat)
+
+##4.Appropriately labels the data set with descriptive variable names.
+
+for (i in 1:length(newcols)) 
+{
+    newcols[i] <- gsub("\\()","",newcols[i])
+    newcols[i] = gsub("-std","StdDev",newcols[i])
+    newcols[i] = gsub("-mean","Mean",newcols[i])
+    newcols[i] = gsub("^(t)","time",newcols[i])
+    newcols[i] = gsub("^(f)","freq",newcols[i])
+    newcols[i] = gsub("([Gg]ravity)","Gravity",newcols[i])
+    newcols[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",newcols[i])
+    newcols[i] = gsub("[Gg]yro","Gyro",newcols[i])
+    newcols[i] = gsub("AccMag","AccMagnitude",newcols[i])
+    newcols[i] = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",newcols[i])
+    newcols[i] = gsub("JerkMag","JerkMagnitude",newcols[i])
+    newcols[i] = gsub("GyroMag","GyroMagnitude",newcols[i])
+}
+
+colnames(meanstddat)<-newcols
+
+##5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+##aggregates variables based on activity type and subject id. 
+agg<-aggregate(.~subjectid+activitytype,meanstddat,mean)
+
+write.table(agg, "tidydat.txt", row.name=FALSE)
